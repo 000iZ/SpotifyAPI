c8db44ab28dbf3a67424f50683b028d58efccb91
Isaac <is.zh@mailfence.com>
2023-03-05 17:21:57 -0800

    Renamed file

--- app_authorization.py
+++ app_authorization.py
@@ -1,9 +1,9 @@
-## for the TEST APP app in Spotify Dashboard
 from requests import post
 import os
 import base64
 import json
 
+## from TEST APP in developer Dashboard
 CLIENT_ID = "5a7aaaee3e1d455c8d8e14d43fc1d1b0"
 CLIENT_SECRET = "8ab3610d092949dd9ebbe6342fdc2e34"
 
@@ -24,9 +24,13 @@ def get_token():
     return token
 
 
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
 def main():
     token = get_token()
-    print(token)
+    auth_header = get_auth_header(token)
 
 
 if __name__ == '__main__':
