5b58dd8c8a54bd52fce69695db81f9144b8fabd4
Isaac <is.zh@mailfence.com>
2023-03-05 17:04:54 -0800

    Defined function to obtain authorization token

---
+++ display_artist_tracks.py
@@ -0,0 +1,28 @@
+## for the TEST APP app in Spotify Dashboard
+from requests import post
+import os
+import base64
+import json
+
+CLIENT_ID = "5a7aaaee3e1d455c8d8e14d43fc1d1b0"
+CLIENT_SECRET = "8ab3610d092949dd9ebbe6342fdc2e34"
+
+def get_token():
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers = headers, data = data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+token = get_token()
+print(token)
