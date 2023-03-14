fdb56b9cf82bb6f396896719b5893f90200e56e0
Isaac <is.zh@mailfence.com>
2023-03-13 17:01:22 -0700

    Formatting changes and additional comments

--- app_authorization.py
+++ app_authorization.py
@@ -5,24 +5,30 @@ import base64
 import json
 
 load_dotenv()
-client_ID = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-print(client_ID, client_secret)
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
 
 def get_token():
-    auth_string = client_ID + ":" + client_secret
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
+    # URL and dictionaries for POST request, as defined in API docs
     url = "https://accounts.spotify.com/api/token"
     headers = {
         "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    data = {"grant_type": "client_credentials"}
+    data = {
+        "grant_type": "client_credentials"
+    }
+
+    # POST request and JSON result, from which a token is obtained
     result = post(url, headers = headers, data = data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
+    json_result = json.loads(result.content)  # a dictionary is returned
+    token = json_result["access_token"]  # 'token_type', 'expires_in' omitted
+
     return token
 
 
@@ -35,5 +41,5 @@ def main():
     auth_header = get_auth_header(token)
 
 
-#if __name__ == '__main__':
-    #main()
+if __name__ == '__main__':
+    main()
